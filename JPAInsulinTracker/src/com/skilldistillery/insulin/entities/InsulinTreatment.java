0442ba19769da987bb7d6ab7898e85f410644462
miiaevia <miiaevia@gmail.com>
2018-05-11 16:58:18 -0600

    fleshed out entity

--- JPAInsulinTracker/src/com/skilldistillery/insulin/entities/InsulinTreatment.java
+++ JPAInsulinTracker/src/com/skilldistillery/insulin/entities/InsulinTreatment.java
@@ -1,5 +1,101 @@
 package com.skilldistillery.insulin.entities;
 
-public class InsulinTreatment {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class InsulinTreatment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
+	
+	private int units;
+	
+	@CreationTimestamp
+	@Temporal(TemporalType.DATE)
+	@Column(name="treatement_date")
+	private Date date; 
+	
+	@CreationTimestamp
+	@Temporal(TemporalType.TIME)
+	@Column(name="treatement_time")
+	private Date time; 
+	
+	private String type;
+	
+	private String brand; 
+	
+	// contstructors
+	public InsulinTreatment() {	}
+
+	public InsulinTreatment(int id, int units, Date date, Date time, String type, String brand) {
+		super();
+		this.id = id;
+		this.units = units;
+		this.date = date;
+		this.time = time;
+		this.type = type;
+		this.brand = brand;
+	}
+	
+	
+	//gets and sets
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUnits() {
+		return units;
+	}
+
+	public void setUnits(int units) {
+		this.units = units;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	
 }
