950dbf84cd45afac4c2ba5f1e3bb5dd60e92656f
miiaevia <miiaevia@gmail.com>
2018-05-14 14:47:37 -0600

    updated README.md, VERY basic

--- InsulinREST/src/main/java/com/skilldistillery/insulin/InsulinRestApplication.java
+++ InsulinREST/src/main/java/com/skilldistillery/insulin/InsulinRestApplication.java
@@ -2,10 +2,16 @@ package com.skilldistillery.insulin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InsulinRestApplication {
-
+public class InsulinRestApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InsulinRestApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(InsulinRestApplication.class, args);
 	}
---
+++ README.md
@@ -0,0 +1,20 @@
+## Event Tracker Project
+
+### Skill Distillery Week 11 Homework
+
+#### Project Description
+This program enables a user to enter and track the date, time and amount of their insulin treatments.
+
+
+#### Technologies used:
+* Java
+* Git
+* Github
+* Spring BOOT
+* REST
+* JPA
+* MySql
+* AWS/EC2
+
+#### Lessons Learned
+* The set up and configuration for the project was much harder than the basic CRUD functionality
