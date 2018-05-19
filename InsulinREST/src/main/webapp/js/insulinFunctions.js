631f14d5675be6695f0453433204b6713714cb78
miiaevia <miiaevia@gmail.com>
2018-05-19 11:04:43 -0600

    read single treatement works, need to add data to be displayed

--- InsulinREST/src/main/webapp/js/insulinFunctions.js
+++ InsulinREST/src/main/webapp/js/insulinFunctions.js
@@ -39,16 +39,146 @@ function getAllTreatments(evt){
 function displayTreatments(treatments){
 	var div = document.createElement('div');
 	document.body.appendChild(div); 
-	var ol = document.createElement('ol'); 
-	div.appendChild(ol);
+	
+	var table = document.createElement('table'); 
+	div.appendChild(table);
+	
+	var thead = document.createElement('thead');
+	var tHeadRow = document.createElement('tr');
+	
+	var tHeadD0 = document.createElement('td');
+	var tHeadD1 = document.createElement('td');
+	var tHeadD2 = document.createElement('td');
+	
+	tHeadD0.textContent = 'ID'; 
+	tHeadD1.textContent = 'Treatement Date';
+	tHeadD2.textContent = 'Insulin Units';
+	
+	tHeadRow.appendChild(tHeadD0); 
+	tHeadRow.appendChild(tHeadD1);
+	tHeadRow.appendChild(tHeadD2);
+	
+	thead.appendChild(tHeadRow);
+	table.appendChild(thead);
+	
+	var tbody = document.createElement('tbody');
 	
 	for(let i = 0; i < treatments.length; i++){
-		var li = document.createElement('li'); 
-		li.textContent = treatments[i].units;
-		ol.appendChild(li);
+		var trow = document.createElement('tr');
+		var td0 = document.createElement('td');
+		var td1 = document.createElement('td');
+		var td2 = document.createElement('td');
+		
+		console.log(treatments[i]);
+		td0.textContent = treatments[i].id;
+		
+		
+		td1.textContent = treatments[i].treatmentDate.substring(0,10);
+		td2.textContent = treatments[i].units;
+		
+		trow.appendChild(td0); 
+		trow.appendChild(td1); 
+		trow.appendChild(td2);
+		console.log()
+		addEventListenerId(td0, treatments[i].id); 
+		tbody.appendChild(trow);
 	}
+	
+	table.appendChild(tbody);
 }
 
+function addEventListenerId(tdId, id){
+//	console.log(tdId);
+	tdId.addEventListener('click', function(e){
+		console.log(id); 
+		getTreatmentById(id); 
+	});
+	}
+
 function displayTreatmentsNotFound(){
-	//get div and add text content
+	var div = document.createElement('div');
+	div.textContent = 'Treatments Not Found'; 
+	document.body.appendChild(div); 
+}
+
+function getTreatmentById(id){
+	// do I want to do evt.preventDefault(); ??? test both ways
+	console.log(id); 
+	var xhr = new XMLHttpRequest();
+	
+	xhr.open('GET', "api/treatments/" + id, true);
+//	console.log(xhr.readyState);
+	
+	xhr.onreadystatechange = function(){
+//		console.log(xhr.readyState); 
+		if (xhr.readyState === 4) {
+			if(xhr.status === 200){
+//				console.log(xhr.readyState);
+				var treatmentJson = this.responseText;
+				var treatment = JSON.parse(treatmentJson); 
+//				console.log(treatment); 
+				displayTreatmentById(treatment); 
+			}
+			else {
+				displayTreatmentNotFound(); 
+			}
+		}
+	}
+	
+	xhr.send(null); 
+}
+
+function displayTreatmentNotFound(){
+	var div = document.createElement('div');
+	div.textContent = 'Treatment Not Found'; 
+	document.body.appendChild(div); 
+}
+
+function displayTreatmentById(treatment) {
+	var div = document.createElement('div'); 
+	
+	var table = document.createElement('table'); 
+	
+	// table head creation
+	var thead = document.createElement('thead');
+	var tHeadRow = document.createElement('tr');
+	
+	var tHD1 = document.createElement('td'); 
+	var tHD2 = document.createElement('td');
+	var tHD3 = document.createElement('td');
+	var tHD4 = document.createElement('td');
+	var tHD5 = document.createElement('td');
+	
+	tHD1.textContent = 'Treatment Date'; 
+	tHD2.textContent = 'Time';
+	tHD3.textContent = 'Units';
+	tHD4.textContent = 'Brand';
+	tHD5.textContent = 'Type';
+	
+	tHeadRow.appendChild(tHD1);
+	tHeadRow.appendChild(tHD2);
+	tHeadRow.appendChild(tHD3);
+	tHeadRow.appendChild(tHD4);
+	tHeadRow.appendChild(tHD5);
+	
+	thead.appendChild(tHeadRow);
+	table.appendChild(thead);
+	
+	//table body creation
+	var tbody = document.createElement('tbody');
+	var trow = document.createElement('tr'); 
+	var td1 = document.createElement('td'); 
+	var td2 = document.createElement('td');
+	
+	td1.textContent = treatment.treatmentDate.substring(0,10);
+	console.log(treatment.treatmentDate); 
+	td2.textConent = treatment.treatmentDate; 
+	
+	trow.appendChild(td1);
+	trow.appendChild(td2); 
+	tbody.appendChild(trow); 
+	table.appendChild(tbody); 
+	
+	div.appendChild(table);
+	document.body.appendChild(div);
 }
